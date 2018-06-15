79a479469b2cce810cb0900b98ae3812f8db1af3
sumatokhinsa <c.c@bk.ru>
2018-06-15 12:41:24 +0300

    Прототип для ssr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
+// angular
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// libs
+import { CookieService } from 'ngx-cookie-service';
+// shared
+import { SharedModule } from '@shared/shared.module';
+import { TranslatesService } from '@shared/translates';
+// components
+import { AppRoutes } from './app.routing';
 import { AppComponent } from './app.component';
 
+export function initLanguage(translateService: TranslatesService): Function {
+  return (): Promise<any> => translateService.initLanguage();
+}
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'my-app' }),
+    HttpClientModule,
+    RouterModule,
+    AppRoutes,
+    BrowserAnimationsModule,
+    SharedModule.forRoot(),
   ],
-  providers: [],
+  declarations: [AppComponent],
+  providers: [
+    CookieService,
+    { provide: APP_INITIALIZER, useFactory: initLanguage, multi: true, deps: [TranslatesService] },
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
