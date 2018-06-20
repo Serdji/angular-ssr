c7c93292401f0a80c55d68e476fdbd77bfcfd476
sumatokhinsa <c.c@bk.ru>
2018-06-20 15:34:26 +0300

    fix

--- api/user/mongoose.js
+++ api/user/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.set('debug', true);
 if ( process.env.ENV === 'dev' ) mongoose.connect('mongodb://localhost/test');
-if ( process.env.ENV === 'prod' ) mongoose.connect('mongodb://Serdjei:S0e1r0e4g8a7@ds263640.mlab.com:63640/mylad123');
+mongoose.connect('mongodb://Serdjei:S0e1r0e4g8a7@ds263640.mlab.com:63640/mylad123');
 console.log(process.env.ENV);
 module.exports = mongoose;
--- src/environments/server/environment.prod.ts
+++ src/environments/server/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   isServer: true,
-  urlApi: 'http://localhost:4000/api',
+  urlApi: 'https://angular-ssr-seo.herokuapp.com/api',
   // for prerender
   host: 'http://localhost:4000'
 };
