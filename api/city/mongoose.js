e2e3a73e638ba45789913764c0b6d5b346792ba6
sumatokhinsa <c.c@bk.ru>
2018-06-20 15:57:22 +0300

    базs нет

--- api/city/mongoose.js
+++ api/city/mongoose.js
@@ -2,5 +2,4 @@ const mongoose = require('mongoose');
 
 mongoose.set('debug', true);
 if ( process.env.ENV === 'dev' ) mongoose.connect('mongodb://localhost/test');
-mongoose.connect('mongodb://Serdjei:S0e1r0e4g8a7@ds263640.mlab.com:63640/mylad123');
 module.exports = mongoose;
--- api/user/mongoose.js
+++ api/user/mongoose.js
@@ -2,6 +2,5 @@ const mongoose = require('mongoose');
 
 mongoose.set('debug', true);
 if ( process.env.ENV === 'dev' ) mongoose.connect('mongodb://localhost/test');
-mongoose.connect('mongodb://Serdjei:S0e1r0e4g8a7@ds263640.mlab.com:63640/mylad123');
 console.log(process.env.NODE_ENV);
 module.exports = mongoose;
