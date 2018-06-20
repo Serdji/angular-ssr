486dc21a5d6ce514dc32a7f184df5c732e277b0c
sumatokhinsa <c.c@bk.ru>
2018-06-20 16:08:17 +0300

    user bd

--- api/user/mongoose.js
+++ api/user/mongoose.js
@@ -2,5 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.set('debug', true);
 if ( process.env.ENV === 'dev' ) mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://admin:admin123@ds263640.mlab.com:63640/mylad123');
 console.log(process.env.NODE_ENV);
 module.exports = mongoose;
