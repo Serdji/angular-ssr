a42a0d496b801ed6dca867a3caf20d8e3823dab5
sergey <c.c@bk.ru>
2018-06-21 22:30:29 +0300

    post

--- api/router.js
+++ api/router.js
@@ -4,11 +4,11 @@ const city = require('./city/index');
 module.exports = (app) => {
 
   app.get('/api/user', user.list);
-  app.get('/api/user/:user', user.send);
+  app.post('/api/user/:user', user.send);
 
   app.get('/api/city', city.list);
   app.get('/api/city/:limit', city.list);
-  app.put('/api/city', city.send);
+  app.post('/api/city', city.send);
   app.delete('/api/city', city.delete);
 
 
